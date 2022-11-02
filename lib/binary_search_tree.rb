c26f5ecda3c97b508980d66ef182a03e8d689652
Shiroyasha21 <red.ezequiel@gmail.com>
2022-11-02 22:14:58 +0800

    write #insert and #find

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -17,11 +17,11 @@ end
 # The Tree class will accept an array
 class Tree
   include MergeSort
-  attr_accessor :arr, :root_node
+  attr_accessor :arr, :root
 
   def initialize(array)
-    @arr = merge_sort(array)
-    @root_node = build_tree(@arr)
+    @arr = merge_sort(array).uniq
+    @root = build_tree(@arr)
   end
 
   def build_tree(array)
@@ -35,12 +35,58 @@ class Tree
 
     Node.new(root, left, right)
   end
+
+  # Thanks fellow TOP student
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.r_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.r_child
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.l_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.l_child
+  end
+
+  def insert(value, node = @root, prev = nil)
+    if node.nil?
+      create_leaf_node(value, prev)
+      return
+    elsif node.l_child.nil? && node.r_child.nil?
+      create_leaf_node(value, node)
+      return
+    end
+
+    if value < node.data
+      insert(value, node.l_child, node)
+    else
+      insert(value, node.r_child, node)
+    end
+  end
+
+  def create_leaf_node(value, node)
+    new_leaf = Node.new(value)
+    if value < node.data
+      node.l_child = new_leaf
+    else
+      node.r_child = new_leaf
+    end
+  end
+
+  # def delete(value)
+  #   node = find(value)
+  # end
+
+  def find(value, node = @root)
+    return false if node.nil?
+    return node if node.data == value
+
+    value < node.data ? find(value, node.l_child) : find(value, node.r_child)
+  end
 end
 
-test = Tree.new([1,3,4,2,5,6,7])
-puts "This is the root node: #{test.root_node} and its data: #{test.root_node.data}"
-puts "This is the left child: #{test.root_node.l_child} and right child: #{test.root_node.r_child}"
-puts "And their respective data: #{test.root_node.l_child.data} & #{test.root_node.r_child.data}"
+test = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+# test = Tree.new([1,3,5,7,8,10]) error test
+test.pretty_print
+test.insert(15)
+test.insert(100)
+test.insert(10)
+test.insert(69)
+test.insert(6999)
 puts ''
-puts "This is their child respectively: #{test.root_node.l_child.l_child} - #{test.root_node.l_child.r_child} AND #{test.root_node.r_child.l_child} - #{test.root_node.r_child.r_child}"
-puts "This is their child data respectively: #{test.root_node.l_child.l_child.data} - #{test.root_node.l_child.r_child.data} AND #{test.root_node.r_child.l_child.data} - #{test.root_node.r_child.r_child.data}"
+test.pretty_print
